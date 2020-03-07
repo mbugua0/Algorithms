688ac65fa3f6968f0f58d157cdc00a4311929306
James Mbugua <58209984+mbugua0@users.noreply.github.com>
2020-03-07 13:54:59 +0300

    Create ArrayListCompare.java

---
+++ Algorithms/src/ArrayListCompare.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArrayListCompare {
+
+    // compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+
+        int len_a = a.size();
+        int len_b = b.size();
+        int score_a = 0;
+        int score_b = 0;
+        List<Integer> result = new ArrayList<>();
+
+        if (len_a == len_b){
+            for(int i = 0; i < len_a; i++){
+                if(a.get(i) > b.get(i)){
+                    score_a = score_a + 1;
+                }
+                else if (a.get(i) == b.get(i)){
+                    score_a = score_a + 0;
+                    score_b = score_b + 0;
+                }else{
+                    score_b = score_b + 1;
+                }
+            }
+        }
+        result.add(0,score_a);
+        result.add(1,score_b);
+        return result;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a.add(aItem);
+        }
+
+        String[] bItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
